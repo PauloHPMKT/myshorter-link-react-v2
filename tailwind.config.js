d068d6d1974d9486588e1ad868776a635ba5d0b8
Paulo Sergio <pauloserg861@gmail.com>
2024-02-24 19:21:25 -0300

    chore: add color to palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        primary: "#134EA8",
+        alpha: "#ffffff26",
+        "alpha-2": "#ffffffcc",
+      },
+    },
   },
   plugins: [],
 };
