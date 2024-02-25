1d9ad5b614ff015687f9104d513490d6d550a3d0
Paulo Sergio <pauloserg861@gmail.com>
2024-02-25 08:24:54 -0300

    feat: add link to redirect to social medias

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -6,13 +6,16 @@ const IconStyles = "hover:text-slate-950 transition-colors duration-300";
 export const Menu = () => {
   return (
     <nav className="flex gap-5 items-center text-white">
-      <a href="#" className={IconStyles}>
+      <a href="https://www.github.com/PauloHPMKT" className={IconStyles}>
         <FiGithub size={18} />
       </a>
-      <a href="" className={IconStyles}>
+      <a href="https://www.intagram.com/tech_devmais" className={IconStyles}>
         <FiInstagram size={18} />
       </a>
-      <a href="" className={IconStyles}>
+      <a
+        href="https://www.linkedin.com/in/paulo-sergio-frontend-dev"
+        className={IconStyles}
+      >
         <FiLinkedin size={18} />
       </a>
       <Link
