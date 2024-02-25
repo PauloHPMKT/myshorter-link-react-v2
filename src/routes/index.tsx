24b93208dc4f48bcd648ed909e5ea46e72893d39
Paulo Sergio <pauloserg861@gmail.com>
2024-02-25 02:07:47 -0300

    feat: add router to Links page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Home } from "../pages/Home";
+import { Links } from "../pages/Links";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/links",
+    element: <Links />,
   },
 ]);
