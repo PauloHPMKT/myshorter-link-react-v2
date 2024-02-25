204fa8a9624e694f8c794ce05377a71b982bcb6a
Paulo Sergio <pauloserg861@gmail.com>
2024-02-25 08:21:59 -0300

    feat: add service to connect supabase

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,3 +1,4 @@
+import { createClient } from "@supabase/supabase-js";
 import axios from "axios";
 
 const baseURL = import.meta.env.VITE_BITLY_URL as string;
@@ -10,3 +11,11 @@ export const api = axios.create({
         Authorization: `Bearer ${key}`,
     },
 });
+
+export const supabase = () => {
+    const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
+    const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+    const supabaseClient = createClient(supabaseUrl, supabaseKey)
+
+    return supabaseClient;
+}
