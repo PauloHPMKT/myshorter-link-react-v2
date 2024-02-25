ac4f29a91b4b1475789fa70039f3c0c4cff047d4
Paulo Sergio <pauloserg861@gmail.com>
2024-02-25 15:28:31 -0300

    refactor: add tailwind to an object

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,27 +1,31 @@
 import { FiGithub, FiInstagram, FiLinkedin } from "react-icons/fi";
 import { Link } from "react-router-dom";
 
-const IconStyles = "hover:text-slate-950 transition-colors duration-300";
+const style = {
+  IconStyles: "hover:text-slate-950 transition-colors duration-300",
+  LinkStyles:
+    "text-slate-950 bg-white py-2 px-6 rounded-md text-[18px] font-bold",
+};
 
 export const Menu = () => {
   return (
     <nav className="flex gap-5 items-center text-white">
-      <a href="https://www.github.com/PauloHPMKT" className={IconStyles}>
+      <a href="https://www.github.com/PauloHPMKT" className={style.IconStyles}>
         <FiGithub size={18} />
       </a>
-      <a href="https://www.intagram.com/tech_devmais" className={IconStyles}>
+      <a
+        href="https://www.instagram.com/tech_devmais"
+        className={style.IconStyles}
+      >
         <FiInstagram size={18} />
       </a>
       <a
         href="https://www.linkedin.com/in/paulo-sergio-frontend-dev"
-        className={IconStyles}
+        className={style.IconStyles}
       >
         <FiLinkedin size={18} />
       </a>
-      <Link
-        to="/links"
-        className="text-slate-950 bg-white py-2 px-6 rounded-md text-[18px] font-bold"
-      >
+      <Link to="/links" className={style.LinkStyles}>
         Meus Links
       </Link>
     </nav>
