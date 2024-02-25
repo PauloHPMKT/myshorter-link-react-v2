923a9ee10049dc2f5749f733c44e6c34c19cc3b8
Paulo Sergio <pauloserg861@gmail.com>
2024-02-25 08:23:41 -0300

    feat: add supabase insert

--- src/services/links.service.ts
+++ src/services/links.service.ts
@@ -1,9 +1,19 @@
-import { api } from "./api";
+import { api, supabase } from "./api";
 
 export default {
     generateShortenLink: (url: string) => {
         return api.post("/shorten", {
             long_url: url,
         });
+    },
+
+    toCreateOnSupabase: async (dataLink: any) => {
+        console.log(dataLink);
+        const { data, error, status } = await supabase().from("shorten-link").insert(dataLink);
+        return {
+            data,
+            error,
+            status
+        };
     }
 }
