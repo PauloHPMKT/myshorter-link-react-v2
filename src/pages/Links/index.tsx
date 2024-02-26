051d1199e0361bc4f88d428f1aa37782b1b107cb
Paulo Sergio <pauloserg861@gmail.com>
2024-02-25 21:31:03 -0300

    feat: add loader to list view

--- src/pages/Links/index.tsx
+++ src/pages/Links/index.tsx
@@ -7,6 +7,7 @@ import { ShortenLinkProps } from "../../types/interfaces";
 
 export const Links = () => {
   const [showModal, setShowModal] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [myLinks, setMyLinks] = useState<ShortenLinkProps[] | null>(null);
   const [data, setData] = useState<ShortenLinkProps | null>(null);
 
@@ -15,8 +16,10 @@ export const Links = () => {
   }, []);
 
   const getLinks = async () => {
+    setIsLoading(true);
     const links = await getShortenLinks();
     setMyLinks(links);
+    setIsLoading(false);
   };
 
   const handleOpenLink = (link: ShortenLinkProps) => {
@@ -39,49 +42,59 @@ export const Links = () => {
             </Link>
             <h1 className="text-white font-semibold text-5xl">Meus Links</h1>
           </div>
-          {myLinks && myLinks.length ? (
-            <ul className="flex flex-col mt-8 gap-2">
-              {myLinks.map((link) => (
-                <li
-                  key={link.id}
-                  className="w-[700px] flex items-center flex-row"
-                >
-                  <button
-                    onClick={() => handleOpenLink(link)}
-                    className="
-                      flex 
-                      border-none 
-                      w-full 
-                      h-12 
-                      bg-alpha 
-                      rounded-md 
-                      min-w-[80%] 
-                      mr-4 
-                      items-center 
-                      px-4 
-                      text-[18px] 
-                      text-white"
-                  >
-                    <FiLink size={18} color="#fff" className="mr-4" />
-                    {link.long_url}
-                  </button>
-                  <button
-                    onClick={() => handleRemoveShortenLink(link.id)}
-                    className="border-none"
-                  >
-                    <FiTrash size={24} color="red" />
-                  </button>
-                </li>
-              ))}
-            </ul>
-          ) : (
+          {isLoading ? (
             <div className="h-[70vh] flex justify-center items-center mt-8 p-12">
               <div>
-                <FiInbox size={150} color="#fff" />
-                <span className="text-[20px] text-white">
-                  Sua lista está vazia...
-                </span>
+                <span className="text-[20px] text-white">Carregando...</span>
               </div>
+            </div>
+          ) : (
+            <div>
+              {myLinks && myLinks.length ? (
+                <ul className="flex flex-col mt-8 gap-2">
+                  {myLinks.map((link) => (
+                    <li
+                      key={link.id}
+                      className="w-[700px] flex items-center flex-row"
+                    >
+                      <button
+                        onClick={() => handleOpenLink(link)}
+                        className="
+                          flex 
+                          border-none 
+                          w-full 
+                          h-12 
+                          bg-alpha 
+                          rounded-md 
+                          min-w-[80%] 
+                          mr-4 
+                          items-center 
+                          px-4 
+                          text-[18px] 
+                          text-white"
+                      >
+                        <FiLink size={18} color="#fff" className="mr-4" />
+                        {link.long_url}
+                      </button>
+                      <button
+                        onClick={() => handleRemoveShortenLink(link.id)}
+                        className="border-none"
+                      >
+                        <FiTrash size={24} color="red" />
+                      </button>
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                <div className="h-[70vh] flex justify-center items-center mt-8 p-12">
+                  <div>
+                    <FiInbox size={150} color="#fff" />
+                    <span className="text-[20px] text-white">
+                      Sua lista está vazia...
+                    </span>
+                  </div>
+                </div>
+              )}
             </div>
           )}
         </div>
