856ec14b8899d564f755178f8066da1e05e0444c
Paulo Sergio <pauloserg861@gmail.com>
2024-05-02 08:53:17 -0300

    refactor: current usability

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -3,24 +3,36 @@ import { useNavigate } from "react-router-dom";
 export const Menu = () => {
   const navigate = useNavigate();
 
-  const navigateToLogin = () => {
-    navigate('/login');
-  }
+  // const navigateToLogin = () => {
+  //   navigate('/login');
+  // }
 
-  const navigateToRegister = () => {
-    navigate('/register');
+  // const navigateToRegister = () => {
+  //   navigate('/register');
+  // }
+
+  const navigateToLinks = () => {
+    navigate('/links');
   }
   return (
     <nav className="flex gap-5 items-center">
-      <button onClick={navigateToLogin} className="font-medium">
-        Login  
-      </button>
       <button 
-        onClick={navigateToRegister}
+        onClick={navigateToLinks}
         className="bg-primary h-[40px] px-5 rounded-2xl text-white font-medium"
       >
-        Crie sua conta
+        Acessar área de links
       </button>
     </nav>
+    // <nav className="flex gap-5 items-center">
+    //   <button onClick={navigateToLogin} className="font-medium">
+    //     Login  
+    //   </button>
+    //   <button 
+    //     onClick={navigateToRegister}
+    //     className="bg-primary h-[40px] px-5 rounded-2xl text-white font-medium"
+    //   >
+    //     Crie sua conta
+    //   </button>
+    // </nav>
   );
 };
