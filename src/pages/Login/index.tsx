273cb0fe6f92b183910e0e725b9a48c1275a4306
Paulo Sergio <pauloserg861@gmail.com>
2024-05-02 07:34:50 -0300

    feat: add login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,39 @@
-export const Login = () => (
-  <div>
-    <h1>Login</h1>
-  </div>
-)
+import { Link } from "react-router-dom";
+import logo from "../../assets/img/shortlify-removebg.png";
+import { useStyle } from "../../hooks/useStyles";
+
+export const Login = () => {
+  const classes = useStyle();
+  return (
+    <div className="h-screen">
+      <div className={`flex items-center flex-col h-screen justify-center ${classes['animation-slidedown']}`}>
+        <div className="mb-10 flex justify-center items-center flex-col">
+          <img src={logo} alt="logo Shortlify" className="w-[200px]" />
+          <div className="flex items-center">
+            <p className="text-lg mt-6 text-slate-600 font-medium">Ainda não possui conta?</p>
+            <Link to="/register" className="text-primary text-lg mt-6 ml-2 font-medium hover:underline">Crie sua conta</Link>
+          </div>
+        </div>
+        <form className="w-[400px] flex flex-col gap-3">
+          <div>
+            <label className="text-md font-semibold">E-mail</label>
+            <input
+              type="text"
+              placeholder="meuemail@example.com"
+              className="w-full h-12 rounded-md text-slate-900 border-2 placeholder-alpha-2 px-2 text-[16px] "
+            />
+          </div>
+          <div className="mb-2">
+            <label className="text-md font-semibold">Senha</label>
+            <input
+              type="password"
+              placeholder="Senha"
+              className="w-full h-12 rounded-md text-slate-900 border-2 placeholder-alpha-2 px-2 text-[16px] "
+            />
+          </div>
+          <button className="bg-primary h-[46px] px-5 rounded-2xl text-white font-medium">Login</button>
+        </form>
+      </div>
+    </div>
+  )
+}
