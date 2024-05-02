58becc5e87f4a43a30a03a0d30444a83b02bafe5
Paulo Sergio <pauloserg861@gmail.com>
2024-05-02 07:31:25 -0300

    style: add style to hook

--- src/hooks/useStyles.ts
+++ src/hooks/useStyles.ts
@@ -1,6 +1,19 @@
 import { createUseStyles } from "react-jss";
 
 export const useStyle = createUseStyles({
+    "@keyframes slidedown": {
+        from: {
+            opacity: 0,
+            transform: "translateY(-90px)",
+        },
+        to: {
+            opacity: 1,
+            transform: "translateY(0)",
+        },
+    },
+    "animation-slidedown": {
+        animation: "$slidedown 2s ease",
+    },
     "@keyframes slideup": {
         from: {
             opacity: 0,
@@ -15,3 +28,4 @@ export const useStyle = createUseStyles({
         animation: "$slideup 0.4s ease",
     },
 });
+
