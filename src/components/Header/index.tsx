40b62b431a41f3f629e58e4ae390d540411d0ea6
Paulo Sergio <pauloserg861@gmail.com>
2024-05-02 08:51:33 -0300

    feat: limit containers

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import logo from "../../assets/img/shortlify-removebg.png";
 
 export const MainHeader = () => {
   return (
-    <header className="w-full h-14 px-40 py-10 flex justify-between items-center">
+    <header className="max-w-[1300px] m-auto h-14 py-10 flex justify-between items-center">
       <div className="flex items-center w-[180px]">
         <img src={logo} alt="shortlify logo" className="w-full" />
       </div>
