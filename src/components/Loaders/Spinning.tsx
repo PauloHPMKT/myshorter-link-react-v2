50aecf69ac13fdd1b808037796335e54ceceb3a6
Paulo Sergio <pauloserg861@gmail.com>
2024-05-03 21:21:29 -0300

    feat: create loader Spinner

---
+++ src/components/Loaders/Spinning.tsx
@@ -0,0 +1,7 @@
+export const Spinning = () => {
+  return (
+    <div className="w-6 h-6 flex justify-center items-center">
+      <div className="animate-spin rounded-full h-6 w-6 border-2 border-t-transparent border-primary"></div>
+    </div>
+  );
+}
