608687467c2b4b8c24659506880155e2c5ec4d6b
Paulo Sergio <pauloserg861@gmail.com>
2024-05-08 19:37:46 -0300

    refactor: add first latter to card profile

--- src/components/Cards/CardProfile.tsx
+++ src/components/Cards/CardProfile.tsx
@@ -2,10 +2,24 @@ import { useContext } from "react"
 import { BiExit } from "react-icons/bi"
 import { AuthContext } from "../../contexts/auth/AuthContext"
 import { useNavigate } from "react-router-dom"
+import { useTruncate } from "../../hooks/useTruncate";
+
+const itemListStyle = `
+  mt-4 
+  flex 
+  items-center 
+  cursor-pointer
+  hover:bg-slate-100 
+  transition
+  duration-350
+  ease-in-out
+  rounded-md
+`;
 
 export const CardProfile = () => {
   const navigate = useNavigate()
-  const { signout } = useContext(AuthContext)
+  const { signout, user } = useContext(AuthContext)
+  const firstLatter = useTruncate(user?.name, 1)
 
   const toSignout = () => {
     signout()
@@ -17,11 +31,11 @@ export const CardProfile = () => {
       <ul>
         <li className="flex items-center border-b-2 pb-4">
           <div className="bg-orange-600 w-10 h-10 flex justify-center items-center rounded-full text-white">
-            H
+            { firstLatter }
           </div>
           <div className="ml-3">
-            <h4>Haliane Pessoa Nascimento</h4>
-            <p className="text-slate-400">halianepessoa20@gmail.com</p>
+            <h4>{ user!.name }</h4>
+            <p className="text-slate-400">{ user!.email }</p>
           </div>
         </li>
         <li className="py-4 border-b-2 flex justify-between">
@@ -30,10 +44,10 @@ export const CardProfile = () => {
             <p className="text-slate-400">Faça um upgrade</p>
           </div>
         </li>
-        <li className="pt-4 flex items-center cursor-pointer">
+        <li className={itemListStyle}>
           <p className="ml-2">Meu perfil</p>
         </li>
-        <li onClick={toSignout} className="pt-4 flex items-center cursor-pointer">
+        <li onClick={toSignout} className={itemListStyle}>
           <BiExit size={20} />
           <p className="ml-2">Sair do workspace</p>
         </li>
