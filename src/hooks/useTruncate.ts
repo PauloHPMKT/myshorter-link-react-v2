514d38d8d3fb9d56e9819a0b51c94d3b75352953
Paulo Sergio <pauloserg861@gmail.com>
2024-05-09 13:56:57 -0300

    fix: treat error length

--- src/hooks/useTruncate.ts
+++ src/hooks/useTruncate.ts
@@ -1,3 +1,3 @@
 export const useTruncate = (text: string | undefined, length: number) => {
-  return text!.length > length ? text!.slice(0, length) : text;
+  return text ? text!.slice(0, length) : text;
 }
