f5a321290ed4bfe037e695fed5badd7c1fcb7b44
Paulo Sergio <pauloserg861@gmail.com>
2024-05-28 20:04:39 -0300

    feat: add submenu bar

--- src/components/Menubar/SidebarLinks.tsx
+++ src/components/Menubar/SidebarLinks.tsx
@@ -1,10 +1,13 @@
 import { Link, useLocation } from "react-router-dom";
 import { LuLayoutDashboard } from "react-icons/lu";
 import { RiScissorsCutFill } from "react-icons/ri";
-import { PiLinkSimpleHorizontalBold  } from "react-icons/pi";
+import { PiLinkSimpleHorizontalBold } from "react-icons/pi";
 import { Each } from "../Each";
 import { createElement, useEffect, useState } from "react";
 import { useStyle } from "../../hooks/useStyles";
+import { FiSettings } from "react-icons/fi";
+import { Sidebar } from "./Sidebar";
+import { BiUser } from "react-icons/bi";
 
 
 
@@ -26,7 +29,7 @@ const menuItems: MenuProps[] = [
     id: "2",
     name: "Meus Links",
     path: "/app/links",
-    icon: () => <PiLinkSimpleHorizontalBold  size={20} />,
+    icon: () => <PiLinkSimpleHorizontalBold size={20} />,
   },
   {
     id: "3",
@@ -34,30 +37,55 @@ const menuItems: MenuProps[] = [
     path: "/app/shorten",
     icon: () => <RiScissorsCutFill  size={20} />,
   },
+  {
+    id: "4",
+    name: "Configurações",
+    path: "#",
+    icon: () => <FiSettings  size={20} />,
+  },
+]
+
+const submenuItems: MenuProps[] = [
+  {
+    id: "1",
+    name: "Perfil",
+    path: "/app/profile",
+    icon: () => <BiUser  size={20} />,
+  },
 ]
 
 
 export const SidebarLinks = () => {
   const classes = useStyle();
   const { pathname } = useLocation();
-  const selectionMenuItem = () => {
-    return menuItems.includes(menuItems.find((item) => item.path === pathname) as MenuProps)
-    ? menuItems.find((item) => item.path === pathname)?.id as string
-    : "1"
+
+  const selectionMenuItem = (items: MenuProps[], index: string) => {
+    return items.find((item) => item.path === pathname)?.id || index;
   }
 
-  const [selected, setSelected] = useState<string>(selectionMenuItem());
+  const [selected, setSelected] = useState<string>(selectionMenuItem(menuItems, "4"));
+  const [selectSubmenu, setSelectSubmenu] = useState<string>(selectionMenuItem(submenuItems, "1"));
+  const [showTeste, setShowTeste] = useState<boolean>(false);
 
   useEffect(() => {
-    setSelected(selectionMenuItem());
+    setSelected(selectionMenuItem(menuItems, "4"));
+    setSelectSubmenu(selectionMenuItem(submenuItems, "1"));
   }, [pathname])
 
   const handleSelected = (id: string) => {
+    if (submenuItems.find((item) => item.id === id)) {
+      setSelectSubmenu("");
+      return;
+    }
     setSelected(id);
   }
 
+  const setSubmenu = () => {
+    setShowTeste(false)
+  }
+
   const renderMenuItem = (link: MenuProps) => {
-    return link.id === selected
+    return link.id === selected;
   }
 
   return (
@@ -73,6 +101,7 @@ export const SidebarLinks = () => {
               {selected && (
                 <Link 
                   to={link.path} 
+                  onClick={() => link.path === '#' && setShowTeste(true)}
                   className={`flex items-center py-4 ${renderMenuItem(link)
                     ? classes.selected
                     : classes.hover
@@ -86,6 +115,37 @@ export const SidebarLinks = () => {
           )}
         />
       </ul>
+      { showTeste && (
+        <Sidebar className={`shadow-3xl absolute top-0 left-20 rounded-md border-0 ${classes["animation-swipe"]}`}>
+          <nav>
+            <ul>
+              <Each 
+                of={submenuItems}
+                render={(link) => (
+                  <li 
+                    key={link.id} 
+                    onClick={() => handleSelected(link.id)}
+                  >
+                    {selectSubmenu && (
+                      <Link 
+                        to={link.path} 
+                        onClick={setSubmenu}
+                        className={`flex items-center py-4 ${renderMenuItem(link)
+                          ? classes.selected
+                          : classes.hover
+                        }`}
+                      >
+                        {createElement(link.icon, { size: 20 })}
+                        <span className="ml-3">{link.name}</span>
+                      </Link>
+                    )}
+                  </li>
+                )}
+            />
+            </ul>
+          </nav>
+        </Sidebar> 
+      )}
     </nav>
   );
 }
