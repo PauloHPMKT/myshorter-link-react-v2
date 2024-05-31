a320a9d5f4aae0151fd8c57c6d7f0b778d62ffed
Paulo Sergio <pauloserg861@gmail.com>
2024-05-31 17:54:52 -0300

    feat: config scroll long url

--- src/components/LinkItem/CardItem/CardLinkItemContent.tsx
+++ src/components/LinkItem/CardItem/CardLinkItemContent.tsx
@@ -10,7 +10,7 @@ export const CardLinkItemContent = ({ title, children, longUrl }: CardLinkItemPr
   return (
     <div className="flex flex-col">
       <h2>{ title }</h2>
-      <span>{longUrl}</span>
+      <span className="mt-4 overflow-x-auto pb-4">{ longUrl }</span>
       <div className="bg-secondary text-white flex justify-between items-center my-4 p-4 border-none h-12 rounded-md cursor-pointer">
         { children }
       </div>
